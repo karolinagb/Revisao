04187cbaa25390b8486ddd1344f1e59cd180a177
Karolina Gomes <63608020+karolinagb@users.noreply.github.com>
2021-04-14 16:48:24 -0300

    Implementacao da Insercao de um Aluno
    Listar Aluno [PENDENTE]

---
+++ Revisao/Revisao/Aluno.cs
@@ -0,0 +1,8 @@
+﻿namespace Revisao
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public decimal Nota { get; set; }
+    }
+}
---
+++ Revisao/Revisao/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
+
+            string opcao = ObterOpcao();
+
+            while (opcao.ToUpper() != "X")
+            {
+                switch (opcao)
+                {
+                    case "1":
+                        //TO DO: adicionar aluno
+                        Console.WriteLine("Informa o nome do aluno:");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno:");
+
+                        if(decimal.TryParse(Console.ReadLine(), out decimal nota)){
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("O valor da nota deve ser decimal");
+                        }
+                        
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+                        break;
+                    case "2":
+                        //TO DO: listar alunos
+                        break;
+                    case "3":
+                        //TO DO: Calcular média geral
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                opcao = ObterOpcao();
+            }
+        }
+
+        private static string ObterOpcao()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcao = Console.ReadLine();
+            Console.WriteLine();
+            return opcao;
+        }
+    }
+}
